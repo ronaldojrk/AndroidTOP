4c5c4927504366c0e9d05f0072336242c0885dc1
unknown <theusmoraes@gmail.com>
2019-11-26 04:38:03 -0300

    Favoritar e Visualizado em funcionamento

--- Movielist/app/src/main/java/com/example/movielist/DetalhesFilme.java
+++ Movielist/app/src/main/java/com/example/movielist/DetalhesFilme.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.squareup.picasso.Picasso;
 
@@ -18,6 +20,9 @@ public class DetalhesFilme extends AppCompatActivity {
     TextView tvNota;
     TextView tvResumo;
     RatingBar ratingBar;
+    ImageView voltar;
+    ToggleButton favorito;
+    ToggleButton visualizado;
     public static final String EXTRA_ALUNO = "aluno";
 
     @Override
@@ -26,17 +31,47 @@ public class DetalhesFilme extends AppCompatActivity {
         setContentView(R.layout.activity_detalhes_filme);
         imageView = findViewById(R.id.imageView2);
         tvAno = findViewById(R.id.tvAno);
-        tvGenero = findViewById(R.id.tvGenero);
         tvResumo = findViewById(R.id.tvResumo);
         tvNota = findViewById(R.id.tvNota);
         ratingBar = findViewById(R.id.ratingBar);
         tvTitulo = findViewById(R.id.tvTitulo);
-        Intent intent = getIntent();
-FilmeParaPassa filme23= (FilmeParaPassa) intent.getSerializableExtra(EXTRA_ALUNO);
-tvTitulo.setText(filme23.title);
+        favorito = findViewById(R.id.button_favorite);
+        visualizado = findViewById(R.id.button_visualizar);
 
-        Picasso.get().load("https://image.tmdb.org/t/p/w500//5myQbDzw3l8K9yofUXRJ4UTVgam.jpg")
+        Intent intent = getIntent();
+        FilmeParaPassa filme23= (FilmeParaPassa) intent.getSerializableExtra(EXTRA_ALUNO);
+        tvTitulo.setText(filme23.title);
+        tvResumo.setText(filme23.overview);
+        tvAno.setText(filme23.releaseDate);
+        tvNota.setText("nota:" + String.valueOf(filme23.voteAverage/2)+"/5");
+        ratingBar.setRating((float) (filme23.voteAverage/2));
+
+        visualizado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        tvTitulo.setText("Vizualizar");
+                    }else{
+
+                    }
+            }
+        });
+        favorito.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    tvTitulo.setText("Favorito");
+                }else{
+                    
+                }
+            }
+        });
+        Picasso.get().load("https://image.tmdb.org/t/p/w500/"+filme23.posterPath)
                 .resize(430,200)
                 .into(imageView);
+
+    }
+    public void voltar(){
+        finish();
     }
 }
